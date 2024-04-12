527a5acd04863458a098076acb11cbf48ef5fbeb
Abhishek Kumar <royabhi647@gmail.com>
2024-04-12 09:25:00 +0530

    menu-items visible only on large screen

--- src/header/index.js
+++ src/header/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 import { VscGrabber, VscClose } from "react-icons/vsc";
 import { Link } from "react-router-dom";
-import { logotext ,socialprofils } from "../content_option";
+import { logotext, socialprofils } from "../content_option";
 import Themetoggle from "../components/themetoggle";
 
 const Headermain = () => {
@@ -31,26 +31,32 @@ const Headermain = () => {
     <>
       <header className="fixed-top site__header">
         <div className="d-flex align-items-center justify-content-between">
-          <Link  className="navbar-brand nav_ac" to="/">
+          <Link className="navbar-brand nav_ac" to="/">
             {logotext}
           </Link>
-          <Link  className="menu-link" to="/portfolio">
-            Project
-          </Link>
-          <Link  className="menu-link" to="/about">
-            About
-          </Link>
-          <Link  className="menu-link" to="/contact">
-            Contact
-          </Link>
+          {
+            !isSmallScreen && (
+              <>
+                <Link className="menu-link" to="/portfolio">
+                  Project
+                </Link>
+                <Link className="menu-link" to="/about">
+                  About
+                </Link>
+                <Link className="menu-link" to="/contact">
+                  Contact
+                </Link>
+              </>
+            )
+          }
           <div className="d-flex align-items-center">
-          <Themetoggle />
-          {isSmallScreen && (
-            <button className="menu__button  nav_ac" onClick={handleToggle}>
-              {!isActive ? <VscClose /> : <VscGrabber />}
-            </button>
-          )}
-          
+            <Themetoggle />
+            {isSmallScreen && (
+              <button className="menu__button  nav_ac" onClick={handleToggle}>
+                {!isActive ? <VscClose /> : <VscGrabber />}
+              </button>
+            )}
+
           </div>
         </div>
 
@@ -60,16 +66,16 @@ const Headermain = () => {
               <div className="menu__container p-3">
                 <ul className="the_menu">
                   <li className="menu_item ">
-                  <Link  onClick={handleToggle} to="/" className="my-3">Home</Link>
+                    <Link onClick={handleToggle} to="/" className="my-3">Home</Link>
                   </li>
                   <li className="menu_item">
-                    <Link  onClick={handleToggle} to="/portfolio" className="my-3"> Project</Link>
+                    <Link onClick={handleToggle} to="/portfolio" className="my-3"> Project</Link>
                   </li>
                   <li className="menu_item">
-                  <Link onClick={handleToggle} to="/about" className="my-3">About</Link>
+                    <Link onClick={handleToggle} to="/about" className="my-3">About</Link>
                   </li>
                   <li className="menu_item">
-                  <Link onClick={handleToggle} to="/contact" className="my-3"> Contact</Link>
+                    <Link onClick={handleToggle} to="/contact" className="my-3"> Contact</Link>
                   </li>
                 </ul>
               </div>
@@ -77,8 +83,8 @@ const Headermain = () => {
           </div>
           <div className="menu_footer d-flex flex-column flex-md-row justify-content-between align-items-md-center position-absolute w-100 p-3">
             <div className="d-flex">
-            <a href={socialprofils.linkedin}>Linkedin</a>
-            <a href={socialprofils.github}>Github</a>
+              <a href={socialprofils.linkedin}>Linkedin</a>
+              <a href={socialprofils.github}>Github</a>
             </div>
             <p className="copyright m-0">copyright __ {logotext}</p>
           </div>
@@ -88,7 +94,7 @@ const Headermain = () => {
       <div className="br-bottom"></div>
       <div className="br-left"></div>
       <div className="br-right"></div>
-      
+
     </>
   );
 };
